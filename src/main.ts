5d44631de2e2beecdfd6f22a2d05f420a96acedb
The-Icaro <icarofigaro100@hotmail.com>
2022-09-14 22:58:28 -0300

    FEAT : Adding Sentry Interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as Sentry from '@sentry/node';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Sentry.init({
+    dsn: '',
+  });
   await app.listen(3000);
 }
 bootstrap();
