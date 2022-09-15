739d7a7dfa417e0f9e7cfe3c75d1331a441c3fa9
The-Icaro <icarofigaro100@hotmail.com>
2022-09-14 22:32:15 -0300

    FEAT : Adding Prometheus Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { PrometheusModule } from './prometheus/prometheus.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, PrometheusModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/prometheus/prometheus.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@nestjs/common';
+import { Registry, collectDefaultMetrics, Histogram, Gauge } from 'prom-client';
+
+export type PrometheusHistogram = Histogram<string>;
+
+interface MapHistogram {
+  [key: string]: Histogram<string>;
+}
+
+interface MapGauge {
+  [key: string]: Gauge<string>;
+}
+
+interface NewMetricRegister {
+  name: string;
+  description: string;
+  labelNames: Array<string>;
+  buckets: Array<number>;
+}
+
+@Injectable()
+export class PrometheusService {
+  private readonly PROMETHEUS_APP_TITLE = 'PrometheusAPP';
+  private readonly PROMETHEUS_APP_PREFIX = 'PrometheusPrefix_';
+  private readonly registry: Registry;
+  private monitoredMetricsHistogram: MapHistogram = {};
+  private monitoredMetricsGauges: MapGauge = {};
+
+  constructor() {
+    this.registry = new Registry();
+    this.registry.setDefaultLabels({ app: this.PROMETHEUS_APP_TITLE });
+    collectDefaultMetrics({
+      register: this.registry,
+      prefix: this.PROMETHEUS_APP_PREFIX,
+    });
+  }
+
+  public get allMetrics(): Promise<string> {
+    return this.registry.metrics();
+  }
+
+  public registerNewMetrics(
+    newMetricRegister: NewMetricRegister,
+  ): Histogram<string> {
+    if (this.monitoredMetricsHistogram[newMetricRegister.name] === undefined) {
+      const histogram = new Histogram({
+        name: newMetricRegister.name,
+        help: newMetricRegister.description,
+        labelNames: newMetricRegister.labelNames,
+        buckets: newMetricRegister.buckets,
+      });
+      this.registry.registerMetric(histogram);
+      this.monitoredMetricsHistogram[newMetricRegister.name] = histogram;
+    }
+
+    return this.monitoredMetricsHistogram[newMetricRegister.name];
+  }
+
+  public registerGauge(name: string, help: string): Gauge<string> {
+    if (this.monitoredMetricsGauges[name] === undefined) {
+      const gauge = (this.monitoredMetricsGauges[name] = new Gauge({
+        name: this.PROMETHEUS_APP_PREFIX + name,
+        help,
+      }));
+      this.registry.registerMetric(gauge);
+      this.monitoredMetricsGauges[name] = gauge;
+    }
+    return this.monitoredMetricsGauges[name];
+  }
+
+  public removeMetricByName(name: string): void {
+    return this.registry.removeSingleMetric(name);
+  }
+
+  public clearAllMetrics(): void {
+    this.registry.resetMetrics();
+    return this.registry.clear();
+  }
+}
