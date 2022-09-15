f9f0ad0784ba20b031138e0155763bff0a90093e
The-Icaro <icarofigaro100@hotmail.com>
2022-09-14 23:28:41 -0300

    FEAT : Done Health Service and Controller

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckResult,
+  HealthCheckService,
+  HttpHealthIndicator,
+} from '@nestjs/terminus';
+import {
+  HealthIndicator,
+  CustomHttpHealthIndicator,
+} from '../health/health-indicator.interface';
+import { PrometheusService } from '../prometheus/prometheus.service';
 
 @Injectable()
-export class HealthService {}
+export class HealthService {
+  private readonly monitoring: Array<HealthIndicator>;
+
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private prometheusService: PrometheusService,
+    private url?: string,
+    private type?: 'Hist' | 'Gauge',
+  ) {
+    this.monitoring = [
+      new CustomHttpHealthIndicator(
+        this.http,
+        this.url,
+        this.type ? this.type : 'Hist',
+        this.prometheusService,
+      ),
+    ];
+  }
+
+  @HealthCheck()
+  public async check(): Promise<HealthCheckResult | undefined> {
+    return await this.health.check(
+      this.monitoring.map((apiIndicator: HealthIndicator) => async () => {
+        try {
+          return await apiIndicator.isHealthy();
+        } catch (error) {
+          Logger.warn(error);
+          return apiIndicator.reportUnhealthy();
+        }
+      }),
+    );
+  }
+}
