f15af7cb1a08f511d58dfff68001629436081447
The-Icaro <icarofigaro100@hotmail.com>
2022-09-14 23:00:15 -0300

    FEAT : Sentry to Capture Exception

--- src/interceptors/sentry/sentry.interceptor.ts
+++ src/interceptors/sentry/sentry.interceptor.ts
@@ -1,9 +1,22 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  CallHandler,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import * as Sentry from '@sentry/minimal';
 
 @Injectable()
 export class SentryInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+    return next.handle().pipe(
+      tap(null, (exception) => {
+        Sentry.captureException(exception);
+      }),
+    );
   }
 }
+
+// For Usage @UseInterceptors(SentryInterceptor) on Desired Controller
